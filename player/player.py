a469e6b56093d4f2da91702816714d31595a9fa4
RedReign <redreigngaming@gmail.com>
2021-08-05 02:27:26 +0100

    fix line 34

--- player/player.py
+++ player/player.py
@@ -31,7 +31,7 @@ class Player(commands.Cog):
             user = ctx.author
         
         
-        data = await self.config.get_raw(user.id)
+        data = await self.config.user(user.id)
         statLines = [
             "**{}** :coin:".format(await self.config.user(user).currentCoins())
             #"Life Points: **{}** / **{}**".format(data.currentLife(), data.maxLife),
