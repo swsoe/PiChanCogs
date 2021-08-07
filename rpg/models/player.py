aa555dac28779e04a6e3c01b73c167770c2b64c7
swsoe <nicholast230@gmail.com>
2021-08-07 16:38:47 -0400

    Update player.py

--- rpg/models/player.py
+++ rpg/models/player.py
@@ -6,12 +6,3 @@ class Player():
         self.coolPoints = coolPoints
         self.coins = coins
         self.xp = xp
-
-    def __str__(self):
-        return "HP: {currentLife}/{maxLife} Cool Points: {coolPoints}, Coins: {coins}, XP: {xp}".format(
-            currentLife = self.currentLife,
-            maxLife = self.maxLife,
-            coolPoints = self.coolPoints,
-            coins = self.coins,
-            xp = self.xp
-        )
