c165e944a8a33e1e7de5d12ea415b7a0813d218f
swsoe <nicholast230@gmail.com>
2021-08-07 18:06:15 -0400

    Update playerCharacter.py

--- rpg/models/playerCharacter.py
+++ rpg/models/playerCharacter.py
@@ -3,7 +3,7 @@ from json import JSONEncoder
 class PlayerCharacter(JSONEncoder):
     
     def default(self, o):   
-            return o.__dict__
+        return o.__dict__
 
     def __init__(self, currentLife: int, maxLife: int, coolPoints: int, coins: int, xp: int):
         self.currentLife = currentLife
