64214d459a717de4258c391125f1c1246684b6df
swsoe <nicholast230@gmail.com>
2021-08-07 17:29:11 -0400

    Update playerCharacter.py

--- rpg/models/playerCharacter.py
+++ rpg/models/playerCharacter.py
@@ -1,3 +1,5 @@
+import json
+
 class PlayerCharacter():
     
     def __init__(self, currentLife: int, maxLife: int, coolPoints: int, coins: int, xp: int):
@@ -6,3 +8,6 @@ class PlayerCharacter():
         self.coolPoints = coolPoints
         self.coins = coins
         self.xp = xp
+
+    def toJson(self):
+        return json.dumps(self, default=lambda o: o.__dict__)
