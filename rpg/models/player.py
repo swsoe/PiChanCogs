c43f2e4f2cd64363fa03bd38ed2c1815e9c396de
swsoe <nicholast230@gmail.com>
2021-08-07 16:08:38 -0400

    Refactored RPG to use player class

--- rpg/models/player.py
+++ rpg/models/player.py
@@ -1,7 +1,17 @@
 class Player():
+    
     def __init__(self, currentLife: int, maxLife: int, coolPoints: int, coins: int, xp: int):
         self.currentLife = currentLife
         self.maxLife = maxLife
         self.coolPoints = coolPoints
         self.coins = coins
         self.xp = xp
+
+    def __str__(self):
+        return "HP: {currentLife}/{maxLife} Cool Points: {coolPoints}, Coins: {coins}, XP: {xp}".format(
+            currentLife = self.currentLife,
+            maxLife = self.maxLife,
+            coolPoints = self.coolPoints,
+            coins = self.coins,
+            xp = self.xp
+        )
--- rpg/rpg.py
+++ rpg/rpg.py
@@ -10,109 +10,108 @@ class RPG(commands.Cog):
     def __init__(self):
         self.config = Config.get_conf(self, 677362587088)
         default_member = {
-            "stats": {
-                "currentLife": 100,
-                "maxLife": 100,
-                "currentCool": 0,
-                "currentCoins": 0,
-                "currentXP": 0,
-                "inventory": {
-                    "trophies": {},
-                    "consumables": {}
-                }
-            }
+            "player" : Player(100, 100, 0, 0, 0)
         }
 
-        default_user = {
-            "player" : Player(100, 100, 0, 0, 0)
-        } 
-
         self.config.register_member(**default_member)
-        self.config.register_user(**default_user)
 
     @commands.command()
     async def stats(self, ctx: commands.context.Context, member: discord.Member = None):
-        if member is None:
-            member = ctx.author
-        
-        data = await self.config.member(member).stats()
-        statLines = [
-            "Life Points: **{}** / **{}**".format(data["currentLife"], data["maxLife"]),
-            "Cool Points: **{}**".format(data["currentCool"]),
-            "", # Compulsory blank line
-            "**{}** XP".format(data["currentXP"]),
-            "",
-            "**{}** :coin:".format(data["currentCoins"])
-        ]
+        try:
+            if member is None:
+                member = ctx.author
+            
+            player = await self.config.member(member).player()
+            statLines = [
+                "Life Points: **{}** / **{}**".format(player.currentLife, player.maxLife),
+                "Cool Points: **{}**".format(player.coolPoints),
+                "", # Compulsory blank line
+                "**{}** XP".format(player.xp),
+                "",
+                "**{}** :coin:".format(player.coins)
+            ]
 
-        titleString = ":trophy: {}'s Statistics :bar_chart:".format(str(ctx.author.display_name))
-        descriptionString = "\n".join(statLines)
-        
-        e = discord.Embed(title=titleString, description=descriptionString)
-        await ctx.send(embed=e)
+            titleString = ":trophy: {}'s Statistics :bar_chart:".format(str(ctx.author.display_name))
+            descriptionString = "\n".join(statLines)
+            
+            e = discord.Embed(title=titleString, description=descriptionString)
+            await ctx.send(embed=e)
+        except Exception as e:
+            await ctx.send("Unexpected error:"+ str(e))
 
     @commands.command()
     async def printstats(self, ctx:commands.context.Context, member:discord.Member = None):
-        if member is None:
-            member = ctx.author
-        
-        titleString = "{}'s Stat Code".format(str(ctx.author.display_name))
-        data = await self.config.member(member).stats()
-        e = discord.Embed(title=titleString, description="```"+str(data)+"```")
-        await ctx.send(embed=e)
+        try:
+            if member is None:
+                member = ctx.author
+            
+            titleString = "{}'s Stat Code".format(str(ctx.author.display_name))
+            player = await self.config.member(member).player()
+            e = discord.Embed(title=titleString, description="```"+str(player)+"```")
+            await ctx.send(embed=e)
+        except Exception as e:
+            await ctx.send("Unexpected error:"+ str(e))
 
     @commands.command(aliases=["beg"])
     async def iamahumblebeggar(self, ctx:commands.context.Context):
-        member = ctx.author
+        try:
+            member = ctx.author
 
-        data = await self.config.member(member).stats()
-        coins = data["currentCoins"]
+            player: Player = await self.config.member(member).player()
+            coins = player.coins
 
-        beggarStrings = [
-            "\"I spare you a crumb.\"",
-            "\"A tuppence for you, my boy!\"",
-            "\"Keep your distance, filth!\" A man you approach throws some money at you before briskly walking away!",
-            "You encounter a dead bourgeois on the side of the road and decide to feast on his flesh."
-        ]
+            beggarStrings = [
+                "\"I spare you a crumb.\"",
+                "\"A tuppence for you, my boy!\"",
+                "\"Keep your distance, filth!\" A man you approach throws some money at you before briskly walking away!",
+                "You encounter a dead bourgeois on the side of the road and decide to feast on his flesh."
+            ]
 
-        nopeStrings = [
-            "You must be **THIS POOR** to receive this handout.",
-            "Not so fast, greedyguts.",
-            "You've got *pleeenty* of money already.",
-            "Did you forget your wallet at home or something?"
-        ]
+            nopeStrings = [
+                "You must be **THIS POOR** to receive this handout.",
+                "Not so fast, greedyguts.",
+                "You've got *pleeenty* of money already.",
+                "Did you forget your wallet at home or something?"
+            ]
 
-        if coins >= 10:
-            randNum = random.randint(0, len(nopeStrings)-1)
-            await ctx.send("{} You must have fewer than **10** :coin: to humbly beg.".format(nopeStrings[randNum]))
-        else:
-            await self.config.member(member).stats.currentCoins.set(10)
-            randNum = random.randint(0, len(beggarStrings)-1)
-            await ctx.send("{} You now have **10** :coin:.".format(beggarStrings[randNum]))
+            if coins >= 10:
+                randNum = random.randint(0, len(nopeStrings)-1)
+                await ctx.send("{} You must have fewer than **10** :coin: to humbly beg.".format(nopeStrings[randNum]))
+            else:
+                player.coins += 10
+                await self.config.member(member).player.set(player)
+                randNum = random.randint(0, len(beggarStrings)-1)
+                await ctx.send("{} You now have **10** :coin:.".format(beggarStrings[randNum]))
+        except Exception as e:
+            await ctx.send("Unexpected error:"+ str(e))
 
     @commands.command()
     async def bankruptme(self, ctx:commands.context.Context):
-        member = ctx.author
-        data = await self.config.member(member).stats()
+        try:
+            member = ctx.author
+            player: Player = await self.config.member(member).player()
 
-        bankruptStrings = [
-            "A hungry ~~bean~~ nature spirit answers your call and consumes your funds!",
-            "You throw your coin purse into the nearest river.",
-            "With great might, you crush your coin purse in the palm of your hand.",
-            "You suddenly discover your money has been replaced with play money."
-        ]
-        
-        randNum = random.randint(0, len(bankruptStrings)-1)
-        await self.config.member(member).stats.currentCoins.set(0)
-        await ctx.send("{} You now have **0** :coin:.".format(bankruptStrings[randNum]))
+            bankruptStrings = [
+                "A hungry ~~bean~~ nature spirit answers your call and consumes your funds!",
+                "You throw your coin purse into the nearest river.",
+                "With great might, you crush your coin purse in the palm of your hand.",
+                "You suddenly discover your money has been replaced with play money."
+            ]
+            
+            randNum = random.randint(0, len(bankruptStrings)-1)
+            player.coins = 0
+            await self.config.member(member).player.set(player)
+            await ctx.send("{} You now have **0** :coin:.".format(bankruptStrings[randNum]))
+        except Exception as e:
+            await ctx.send("Unexpected error:"+ str(e))
 
     @commands.command()
     async def addxp(self, ctx: commands.context.Context, xp: int):
         try:
-            data = await self.config.member(ctx.author).stats()
-            data["currentXP"] = data["currentXP"] + xp
-            await ctx.send(data["currentXP"])
-            await self.config.member(ctx.author).stats.set(data)
+            player: Player = await self.config.member(ctx.author).player()
+            player.xp += xp
+            await ctx.send(player.xp)
+            await self.config.member(ctx.author).player.set(player)
         except Exception as e:
             await ctx.send("Unexpected error:"+ str(e))
 
@@ -127,7 +126,7 @@ class RPG(commands.Cog):
     @commands.command()
     async def printuserstats(self, ctx: commands.context.Context):
         try:
-            player = await self.config.user(ctx.author).player()
+            player: Player = await self.config.user(ctx.author).player()
             await ctx.send(player.currentLife)
         except Exception as e:
             await ctx.send("Unexpected error:"+ str(e))
