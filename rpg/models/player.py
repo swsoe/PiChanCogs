4a0e495ecd5206d8cf20012867c568110cc090d0
swsoe <nicholast230@gmail.com>
2021-08-07 19:52:21 -0400

    Update player.py

--- rpg/models/player.py
+++ rpg/models/player.py
@@ -10,5 +10,5 @@ class Player():
         return await self.rpgCore.config.member(self.member).player()
 
     async def GetCurrentLife(self) -> int:
-        self.rpgCore.config
-        return await self.GetPlayer()["currentLife"]
+        playerData = await self.GetPlayer()
+        return playerData["currentLife"]
