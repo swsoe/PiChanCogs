356c72a879a4b956c3af81d5a043e94a743ab425
swsoe <nicholast230@gmail.com>
2022-03-28 23:55:14 -0400

    Start of cataloging

--- jukebox/jukebox.py
+++ jukebox/jukebox.py
@@ -2,6 +2,7 @@ from email.policy import default
 from redbot.core import Config
 from redbot.core import commands
 import discord
+import re
 
 class Jukebox(commands.Cog):
 
@@ -23,15 +24,27 @@ class Jukebox(commands.Cog):
     async def ParseChannel(self, ctx: commands.context.Context):
         try:
             channelID = await self.config.guild(ctx.guild).channelID()
+            regex = "^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
             if channelID is None:
                 await ctx.send("Channel is not set")
             else:
                 jukeboxChannel = await ctx.bot.fetch_channel(channelID)
                 await ctx.send("Parsing messages in : " + jukeboxChannel.name)
 
-                messages = await jukeboxChannel.history(oldest_first=True).flatten()
+                messages = await jukeboxChannel.history(limit=10, oldest_first=False).flatten()
 
-                await ctx.send(messages[-1].content)
+                #await ctx.send(messages[-1].content)
+                links = []
+                m: discord.Message
+                for m in messages:
+                    if m.content is None or re.search(regex, m.content) is None:
+                        await m.add_reaction("❌")
+                    else:
+                        match = re.search(regex, m.content)
+                        links.append(match.group())
+                        await m.add_reaction("✅")
+                
+                await ctx.send(links)
                 
         except BaseException as ex:
             await ctx.send(str(ex))
