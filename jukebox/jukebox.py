72653a62eb8a6f08516c9a1f7bf1dcfab7508271
Nicholas Thompson <nicholast230@gmail.com>
2022-03-28 22:41:45 -0400

    Started work on jukebox cog

--- jukebox/jukebox.py
+++ jukebox/jukebox.py
@@ -1,13 +1,28 @@
-from typing import ContextManager
+from email.policy import default
+from redbot.core import Config
 from redbot.core import commands
 import discord
 
 class Jukebox(commands.Cog):
 
     def __init__(self, bot):
-        self.bot = bot
+        self.config = Config.get_conf(self, 159753258460)
+        default_guild = {
+            "channelID": ""
+        }
+        self.config.register_guild(**default_guild)
 
     @commands.command()
-    async def mycom(self, ctx: commands.context.Context):
-        ctx.send()
-        await ctx.send("I can do stuff!")
+    async def SetChannel(self, ctx: commands.context.Context, channel):
+        await self.config.guild(ctx.guild).channelID.set(channel)
+        await ctx.send("Channel set to: " + channel)
+
+
+    @commands.command()
+    async def ParseChannel(self, ctx: commands.context.Context):
+        channelID = self.config.guild(ctx.guild).channelID()
+        if channelID is None:
+            await ctx.send("Channel is not set")
+        else:
+            jukeboxChannel = discord.TextChannel(discord.Client.get_channel(channelID))
+            await ctx.send(jukeboxChannel.type)
